4e93b872633403033b3b857a75812a640e9173dd
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-11-21 18:06:43 +0200

    022-where2.sql added

---
+++ sql/022-where2.sql
@@ -0,0 +1,13 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-11-21
+-- File: 022-where2.sql
+
+
+SELECT ename AS "Employee Name"
+       , deptno AS "Department Number"
+FROM emp
+WHERE empno = 7566
+;
+
+-- End of file
+
