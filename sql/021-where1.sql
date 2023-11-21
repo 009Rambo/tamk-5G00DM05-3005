582426e9bde8756e541eba3b1f37e05beb743168
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-11-21 17:59:33 +0200

    021-where1.sql added

---
+++ sql/021-where1.sql
@@ -0,0 +1,12 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-11-21
+-- File: 021-where1.sql
+
+SELECT ename AS "Employee Name"
+       , sal AS "Salary"
+FROM   emp
+WHERE sal > 2850
+;
+
+-- End of file
+
