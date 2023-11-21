046d2dab109671590da4abccc64d7f060ef453e3
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-11-21 11:38:10 +0200

    013-query1 added

---
+++ sql/013-query1.sql
@@ -0,0 +1,13 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-11-21
+-- File: 013-query1.sql
+
+SELECT ename AS "Name",
+       job AS "Job",
+       hiredate AS "Hiring Date",
+       empno AS "Employee Number"
+FROM emp;
+
+
+-- End of file
+
