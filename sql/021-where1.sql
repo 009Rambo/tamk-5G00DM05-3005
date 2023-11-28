7c5d3320b9e19f87fb23a334a56fb5ca2d9de5ee
Ramesh Pandey <ramesh.pandey@tuni.fi>
2023-11-29 00:58:40 +0200

    021-where1.sql corrected

--- sql/021-where1.sql
+++ sql/021-where1.sql
@@ -2,8 +2,9 @@
 -- Date: 2023-11-21
 -- File: 021-where1.sql
 
-SELECT ename AS "Employee Name"
-       , sal AS "Salary"
+SELECT ename
+       AS "ename"
+       , sal AS "sal"
 FROM   emp
 WHERE sal > 2850
 ;
