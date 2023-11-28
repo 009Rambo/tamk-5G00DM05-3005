3857fa439ab95e62e8ad1ad5787135b09ac59e6e
Ramesh Pandey <ramesh.pandey@tuni.fi>
2023-11-28 23:43:00 +0200

    014-query2.sql corrected

--- sql/014-query2.sql
+++ sql/014-query2.sql
@@ -2,14 +2,13 @@
 -- Date: 2023-11-21
 -- File: 014-query2.sql
 
-SELECT
-  ename AS "Name",
-  (CASE
+SELECT ename AS "ename"
+  , (CASE
     WHEN strftime('%m', hiredate) = '01' THEN sal
     WHEN strftime('%m', hiredate) = '02' THEN sal
     WHEN strftime('%m', hiredate) = '03' THEN sal
     ELSE 0
-  END) AS "First Quarter Salary"
+  END) AS "first quarter"
 FROM emp;
 
 
