6ffdf1340cee1df0cfb8b8e7f7a9ffcd6e7fe6fd
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 00:26:17 +0200

    031-order-by1.sql added

---
+++ sql/031-order-by1.sql
@@ -0,0 +1,20 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-08
+-- File: 031-order-by1.sql
+
+
+SELECT
+    ename AS "ename",
+    sal AS "sal",
+    sal * 1.15 AS "new salary"
+FROM
+    emp
+WHERE
+    mgr = (SELECT empno FROM emp WHERE ename = 'BLAKE')
+ORDER BY
+    "new salary" ASC,
+    "ename" ASC;
+
+
+-- End of file
+
