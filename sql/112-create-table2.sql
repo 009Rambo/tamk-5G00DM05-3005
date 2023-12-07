c9f988176854aa5aae9f4fd349c7a306eac6bccc
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 01:31:26 +0200

    112-create-table2.sql added

---
+++ sql/112-create-table2.sql
@@ -0,0 +1,17 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-07
+-- File: 112-create-table2.sql
+
+CREATE TABLE football (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Coach_first_name TEXT,
+    coach_last_name TEXT,
+    team TEXT,
+    comment TEXT
+);
+
+
+
+-- End of file
+
+
