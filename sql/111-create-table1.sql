74b50e8f4cf025a312352861becbb0cf2b4741db
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 01:47:40 +0200

    111-create-table1.sql corrected

--- sql/111-create-table1.sql
+++ sql/111-create-table1.sql
@@ -3,14 +3,15 @@
 -- File: 111-create-table1.sql
 
 CREATE TABLE person (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    last_name VARCHAR,
-    first_name VARCHAR,
-    phone VARCHAR CHECK (phone GLOB '+[ 0-9]*' AND LENGTH(phone) >= 2),
-    zip VARCHAR,
-    city VARCHAR,
-    address VARCHAR
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    last VARCHAR(255),
+    first VARCHAR(255),
+    phone VARCHAR(20) CHECK (phone GLOB '+[ 0-9]*' AND LENGTH(phone) >= 2),
+    zip VARCHAR(10),
+    city VARCHAR(255),
+    address VARCHAR(255)
 );
+
 
 
 -- End of file
