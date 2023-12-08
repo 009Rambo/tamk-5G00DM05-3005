bf745fb3ce7d46ac49b08b4db19bb0b8d3acf6ed
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 02:30:44 +0200

    121-insert-table1.sql added

---
+++ sql/121-insert-table1.sql
@@ -0,0 +1,14 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-07
+-- File: 121-insert-table1.sql
+
+INSERT INTO person (id, last, first, phone, zip, city, address) VALUES
+(1, 'Doe', 'John', '123455', '11122', 'New York', '2th avenue'),
+(2, 'Jordan', 'Mike', '221122', '01212', 'Washington', 'South Park 3'),
+(3, 'Durak', 'Stephen', '550011', '55654', 'Miami', 'Sea Drive 112');
+
+
+
+-- End of file
+
+
