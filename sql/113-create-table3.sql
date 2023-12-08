5cec33f87df90ae9bd4ed7ab272f3a4f6b5b2edf
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 03:29:46 +0200

    113-create-table3.sql again corrected

--- sql/113-create-table3.sql
+++ sql/113-create-table3.sql
@@ -3,11 +3,11 @@
 -- File: 113-create-table3.sql
 
 CREATE TABLE weather (
-    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
-    time_of_reading TEXT,
-    high FLOAT INTEGER,
-    low FLOAT INTEGER,
-    sig VARCHAR(2),
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    time_of_reading DATETIME,
+    high NUMERIC(3,1),
+    low NUMERIC(3,1),
+    sig TEXT(2),
     comment VARCHAR(255)
 );
 
