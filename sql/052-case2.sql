45d7eca8e3476d96696e0817807dfe27e5e68c87
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 06:49:41 +0200

    052-case2.sql corrected

--- sql/052-case2.sql
+++ sql/052-case2.sql
@@ -3,20 +3,17 @@
 -- File: 052-case2.sql
 
 
-UPDATE emp
-SET comm = CASE
-    WHEN deptno = 30 AND (comm IS NULL OR comm = 0) THEN 100
-    WHEN deptno = 30 AND comm IS NOT NULL THEN comm * 1.05
-    ELSE comm
-END;
-
 SELECT
     ename AS "ename",
-    comm AS "comm"
+    CASE
+        WHEN deptno = 30 AND (comm IS NULL OR comm = 0) THEN 100
+        WHEN deptno = 30 AND comm IS NOT NULL THEN comm * 1.05
+        ELSE comm
+    END AS "comm"
 FROM
     emp
 ORDER BY
-    comm ASC,
+    "comm" ASC,
     ename ASC;
 
 
