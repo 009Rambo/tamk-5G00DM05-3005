b34c6be935ded906c40596ff2db51ebfb970fee2
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-08 02:56:35 +0200

    122-insert-table2.sql added

---
+++ sql/122-insert-table2.sql
@@ -0,0 +1,13 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-07
+-- File: 122-insert-table2.sql
+
+INSERT INTO football (id, first, last, team, comment) VALUES
+(1, 'Jim', 'Anders', 'Manchester', 'UPS 10'),
+(2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
+(3, 'Jenny', 'Richards', 'Highland', 'Scottish national');
+
+
+-- End of file
+
+
