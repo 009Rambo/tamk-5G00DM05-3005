94ffe7aeafa3e144bd39d7e1592b6810e2d0a78b
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-23 13:53:58 +0200

    103-multiple-tables-self-join3.sql added

---
+++ sql/103-multiple-tables-self-join3.sql
@@ -0,0 +1,18 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-23
+-- File: 103-multiple-tables-self-join3.sql
+
+SELECT DISTINCT
+    emp.ename AS "Employee Name"
+FROM
+    emp
+JOIN
+    emp manager ON emp.mgr = manager.empno
+WHERE
+    emp.sal > manager.sal
+   AND emp.comm IS NULL
+ORDER BY
+    "Employee Name" ASC;
+
+-- End of file
+
