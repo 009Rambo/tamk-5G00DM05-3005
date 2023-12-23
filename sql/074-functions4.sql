deeb9686b9ef50d109b0bb4c210bcaf7cee977d6
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-23 23:26:43 +0200

    074-functions4.sql corrected

--- sql/074-functions4.sql
+++ sql/074-functions4.sql
@@ -1,15 +1,13 @@
 -- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
 -- Date: 2023-12-23
 -- File: 074-functions4.sql
-
 SELECT
-    CASE WHEN ename LIKE 'a%' OR ename LIKE 'm%' OR ename LIKE 'j%' THEN LOWER(ename) ELSE ename END AS ename,
+    ename,
     CASE WHEN ename LIKE 'a%' OR ename LIKE 'm%' OR ename LIKE 'j%' THEN LOWER(job) ELSE job END AS job
 FROM
     emp
 ORDER BY
     LOWER(ename) ASC, LOWER(job) ASC;
-
 
 
 -- End of file
