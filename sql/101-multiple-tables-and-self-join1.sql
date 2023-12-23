b285905d4aea1e5f98964d425ffceab0c9d6a6eb
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-23 13:50:53 +0200

    101-multiple-tables-and-self-join1.sql added

---
+++ sql/101-multiple-tables-and-self-join1.sql
@@ -0,0 +1,20 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-23
+-- File: 101-multiple-tables-self-join1.sql
+
+SELECT
+    mgr.ename AS "Manager",
+    mgr.empno AS "Mgr#",
+    emp.ename AS "Employee",
+    emp.empno AS "Emp#"
+FROM
+    emp mgr
+JOIN
+    emp emp ON mgr.empno = emp.mgr
+WHERE
+    mgr.ename IN ('BLAKE', 'FORD', 'SCOTT')
+ORDER BY
+    "Manager" ASC, "Employee" ASC;
+
+-- End of file
+
