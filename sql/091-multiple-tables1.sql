9f89a9f62dba7810dabb192c94c4a09793faf18a
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-23 13:28:53 +0200

    091-multiple-tables1.sql added

---
+++ sql/091-multiple-tables1.sql
@@ -0,0 +1,19 @@
+-- Author: Ramesh Pandey <ramesh.pandey@tuni.fi>
+-- Date: 2023-12-23
+-- File: 091-multiple-tables1.sql
+
+SELECT
+    e.ename AS "Employee Name",
+    e.deptno AS "Department Number",
+    LOWER(d.dname) AS "Department Name"
+FROM
+    emp e
+JOIN
+    dept d ON e.deptno = d.deptno
+WHERE
+    LOWER(d.loc) = 'dallas' OR UPPER(e.job) = 'SALESMAN'
+ORDER BY
+    "Employee Name" ASC;
+
+-- End of file
+
