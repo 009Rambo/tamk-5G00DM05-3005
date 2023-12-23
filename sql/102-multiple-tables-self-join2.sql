18b061b2349e31b1980239c32d74112a8423624a
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-23 23:08:41 +0200

    102-multiple-tables-self-join2.sql corrected

--- sql/102-multiple-tables-self-join2.sql
+++ sql/102-multiple-tables-self-join2.sql
@@ -12,11 +12,11 @@ FROM (
         CASE WHEN e1.ename < e2.ename THEN e1.ename ELSE e2.ename END AS "Employee",
         CASE WHEN e1.ename < e2.ename THEN e2.ename ELSE e1.ename END AS "Colleague"
     FROM
-        emp e
+        emp AS e
     JOIN
-        emp e1 ON e.deptno = e1.deptno
+        emp AS e1 ON e.deptno = e1.deptno
     JOIN
-        emp e2 ON e.deptno = e2.deptno
+        emp AS e2 ON e.deptno = e2.deptno
     WHERE
         e1.empno < e2.empno
 ) AS T
