cc65300f728608b05a5d79dc2d866f35a7c74cff
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-23 13:59:24 +0200

    083-group-functions3.sql corrected

--- sql/083-group-functions3.sql
+++ sql/083-group-functions3.sql
@@ -3,15 +3,13 @@
 -- File: 083-group-functions3.sql
 
 SELECT
-    deptno AS "Department",
     MAX(sal) AS "high",
     MIN(sal) AS "low"
 FROM
     emp
 WHERE
-    deptno IN (10, 30)
-GROUP BY
-    deptno;
+    deptno IN (10, 30);
+
 
 
 -- End of file
