d4b3a81c8e52ca9167613f3512d69f0460042481
Ramesh Pandey <ramesh.pandey.tuni.fi>
2023-12-24 04:58:30 +0200

    113-create-table3.sql corrected

--- sql/113-create-table3.sql
+++ sql/113-create-table3.sql
@@ -4,11 +4,11 @@
 
 CREATE TABLE weather (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
-    time_of_reading TEXT,
-    high REAL,
-    low REAL,
+    time_of_reading NUMERIC,
+    high NUMERIC,
+    low NUMERIC,
     sig TEXT(2),
-    comment TEXT
+    comment VARCHAR(255) NOT NULL
 );
 
 
